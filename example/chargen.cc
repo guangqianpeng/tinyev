5325f466a5c80be4f4c3f6966fe7113e07c2c145
frank <625727796@qq.com>
2017-11-29 11:46:35 +0800

    rename to example

---
+++ example/chargen.cc
@@ -0,0 +1,71 @@
+//
+// Created by frank on 17-9-3.
+//
+
+#include "Logger.h"
+#include "EventLoop.h"
+#include "TcpConnection.h"
+#include "TcpServer.h"
+
+using namespace tinyev;
+
+class ChargenServer
+{
+public:
+    ChargenServer(EventLoop* loop, const InetAddress& addr)
+            : server_(loop, addr),
+              transfered_(0)
+    {
+        server_.setConnectionCallback(std::bind(
+                &ChargenServer::onConnection, this, _1));
+        server_.setWriteCompleteCallback(std::bind(
+                &ChargenServer::onWriteComplete, this, _1));
+
+        std::string line;
+        for (int i = 33; i < 127; ++i)
+            line.push_back(char(i));
+        line += line;
+
+        for (size_t i = 0; i < 127-33; ++i)
+            message_ += line.substr(i, 72) + '\n';
+        transfered_ += message_.size();
+    }
+
+    void start() { server_.start(); }
+
+    void onConnection(const TcpConnectionPtr& conn)
+    {
+        INFO("connection %s is [%s]",
+             conn->name().c_str(),
+             conn->connected() ? "up":"down");
+
+        if (conn->connected()) {
+            conn->send("[tinyev chargen server]\n");
+            conn->send(message_);
+        }
+        else {
+            INFO("%ld bytes transfered", transfered_);
+        }
+    }
+
+    void onWriteComplete(const TcpConnectionPtr& conn)
+    {
+        conn->send(message_);
+        transfered_ += message_.size();
+    }
+
+private:
+    TcpServer server_;
+    size_t transfered_;
+    std::string message_;
+};
+
+int main()
+{
+    setLogLevel(LOG_LEVEL_TRACE);
+    EventLoop loop;
+    InetAddress addr(9877);
+    ChargenServer server(&loop, addr);
+    server.start();
+    loop.loop();
+}
---
+++ example/timer.cc
@@ -0,0 +1,28 @@
+//
+// Created by frank on 17-11-17.
+//
+
+#include <cstdio>
+
+#include "Logger.h"
+#include "EventLoop.h"
+
+using namespace tinyev;
+
+int main()
+{
+    EventLoop loop;
+
+    loop.runEvery(1s, [](){
+       INFO("run every 500ms");
+    });
+    loop.runAfter(5s, [&](){
+        INFO("end after 1min");
+        loop.quit();
+    });
+    loop.runAt(Clock::nowAfter(15min), [&](){
+        INFO("run 15min later");
+    });
+    INFO("start loop");
+    loop.loop();
+}
