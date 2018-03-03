e734764f569fd42262960cda3384368032c81f74
frank <625727796@qq.com>
2018-03-03 22:00:54 +0800

    beautify code

--- example/timer.cc
+++ example/timer.cc
@@ -14,7 +14,7 @@ int main()
     EventLoop loop;
 
     loop.runEvery(1s, [](){
-       INFO("run every 1s");
+        INFO("run every 1s");
     });
     loop.runAfter(10s, [&](){
         INFO("end after 10s");
@@ -23,6 +23,7 @@ int main()
     loop.runAt(Clock::nowAfter(15min), [&](){
         INFO("run 15min later");
     });
-    INFO("start loop");
+
+    INFO("start looping");
     loop.loop();
 }
